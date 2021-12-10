2be8de37110b63444b49c02e73c975a112237503
Flávio Arêas <vflavio9@gmail.com>
2021-12-10 20:50:01 -0300

    test: create user tests

---
+++ backend/src/__tests__/createUser.spec.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import knex from '../database/connection';
+
+import app from '../app';
+
+describe('POST /users', function () {
+  beforeAll(async () => {
+    await knex('users').where('name', 'John Doe').del();
+  });
+
+  it('should be able to create a new user', function (done) {
+    request(app)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it('should not be able to create a new user without password', function (done) {
+    request(app)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it("should not be able to create an user with an email that's already in use", function (done) {
+    request(app)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
